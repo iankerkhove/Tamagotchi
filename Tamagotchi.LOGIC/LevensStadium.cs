a5288c15e0d70727b9d91b84a884177a294d0f7b
iankerkhove <ian.kerkhove@student.ehb.be>
2017-10-23 22:25:06 +0200

    logic
    adding logic and solution file

---
+++ Tamagotchi.LOGIC/LevensStadium.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.LOGIC
+{
+    public enum LevensStadium
+    {
+        EI, BABY, KIND, PUBER, VOLWASSEN, SENIOR, DOOD
+    }
+}
